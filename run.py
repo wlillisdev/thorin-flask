100794221a6a676cb881458dcb0356ba61b3e5a3
wlillisdev <williamlillis100@gmail.com>
2022-05-09 18:51:17 +0000

    bootstrap theme added

--- run.py
+++ run.py
@@ -16,7 +16,12 @@ def about():
 
 @app.route("/contact")
 def contact():
-    return render_template("contact.html")    
+    return render_template("contact.html")  
+
+
+@app.route("/careers")
+def careers():
+    return render_template("careers.html")  
 
 
 if __name__ == "__main__":
